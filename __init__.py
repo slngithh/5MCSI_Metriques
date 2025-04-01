23531397a04cee38eed02e5547432dbb526ccc75
slngithh <solene.bernabe@edu.esiee-it.fr>
2025-04-01 15:01:46 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -37,35 +37,6 @@ def mongraphique():
 def histo():
     return render_template('histogramme.html')
 
-@app.route('/extract-minutes/<date_string>')
-def extract_minutes(date_string):
-    date_object = datetime.strptime(date_string, '%Y-%m-%dT%H:%M:%SZ')
-    minutes = date_object.minute
-    return jsonify({'minutes': minutes})
-
-@app.route('/commits/')
-def show_commit_graph():
-    url = "https://api.github.com/repos/slngithh/5MCSI_Metriques/commits"
-    response = requests.get(url)
-
-    #if response.status_code != 200:
-     #   return f"Erreur de récupération des données: {response.status_code}", 500
-
-    commits = response.json()
-    minute_counts = Counter()
-
-    for commit in commits:
-        try:
-            date_str = commit['commit']['author']['date']
-            minute = datetime.strptime(date_str, '%Y-%m-%dT%H:%M:%SZ').minute
-            minute_counts[minute] += 1
-        except Exception as e:
-            continue  # En cas d'erreur sur un commit, on saute
-          
-    minutes = list(range(60))
-    counts = [minute_counts.get(minute, 0) for minute in minutes]
-
-    return render_template('commits.html', labels=minutes, values=counts)
 
 if __name__ == "__main__":
   app.run(debug=True)
